b26d08ec32ea216961d6be14033af62a4cd05540
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-10 09:46:07 -0400

    added a catch for errors

--- explore.js
+++ explore.js
@@ -1,14 +1,13 @@
 const Twilio = require("twilio");
 
 const client = new Twilio(
-    "AuthSid",
-    "AuthToken"
+    
 )
 
 client.messages
     .list()
     .then(messages=>
         console.log(`The most recent message is ${messages[0].body}`)
-        );
+        ).catch(err = console.error(err));
 
 console.log("Gathering message log");
