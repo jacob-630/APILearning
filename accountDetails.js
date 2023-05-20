64265a501dfdfa6d423a2803c6b63fec5a1ddcd1
Jacob Vaanholt <Jacobvaanholt7@gmail.com>
2023-05-19 21:08:35 -0400

    worked with js API's

---
+++ accountDetails.js
@@ -0,0 +1,2 @@
+const authSid = "ACa6d1b77d69ba2bdd0b326a065465b1cf";
+const authTok = "751c540ed780b1ec44733cb7f770600e";
--- explore.js
+++ explore.js
@@ -1,7 +1,8 @@
 const Twilio = require("twilio");
 
 const client = new Twilio(
-    
+    "ACa6d1b77d69ba2bdd0b326a065465b1cf",
+    "751c540ed780b1ec44733cb7f770600e"
 )
 
 client.messages
